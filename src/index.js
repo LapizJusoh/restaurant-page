975246cc6d37a08875d9bfe38e76b93ad349fc69
Bob <lapizjusoh@yahoo.com>
2025-01-13 13:57:35 +0800

    Made content entirely in javascript

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import "./styles.css";
 
-console.log("Hello, world!")
+(() => {
+  const content = document.querySelector("#content");
+  const div = document.createElement("div");
+  const p = document.createElement("p");
+  
+  div.className = "stuff";
+  p.innerHTML = `This is Home!`;
+  content.appendChild(div);
+  div.appendChild(p);
+})();
+
+(() => {
+  const content = document.querySelector("#content");
+  const div = document.createElement("div");
+  const p = document.createElement("p");
+  
+  div.className = "stuff";
+  p.innerHTML = `This is Menu!`;
+  content.appendChild(div);
+  div.appendChild(p);
+})();
+
+(() => {
+  const content = document.querySelector("#content");
+  const div = document.createElement("div");
+  const p = document.createElement("p");
+  
+  div.className = "stuff";
+  p.innerHTML = `This is Contact Us!`;
+  content.appendChild(div);
+  div.appendChild(p);
+})();
