3a3943afb36634bc4db934cc73ee4e44ee319937
Bob <lapizjusoh@yahoo.com>
2025-01-15 10:33:31 +0800

    Active button persist after click

--- src/index.js
+++ src/index.js
@@ -10,17 +10,28 @@ const menuBtn = document.querySelector("#menu-btn");
 const contactBtn = document.querySelector("#contact-btn");
 
 addHome();
+homeBtn.classList.toggle("active");
+
 homeBtn.addEventListener("click", () => {
   content.removeChild(content.lastElementChild);
   addHome();
+  !homeBtn.classList.contains("active") ? homeBtn.classList.toggle("active") : '' ;
+  menuBtn.classList.contains("active") ? menuBtn.classList.toggle("active") : '' ;
+  contactBtn.classList.contains("active") ? contactBtn.classList.toggle("active") : '' ;
 });
 
 menuBtn.addEventListener("click", () => {
   content.removeChild(content.lastElementChild);
   addMenu();
+  !menuBtn.classList.contains("active") ? menuBtn.classList.toggle("active") : '' ;
+  homeBtn.classList.contains("active") ? homeBtn.classList.toggle("active") : '' ;
+  contactBtn.classList.contains("active") ? contactBtn.classList.toggle("active") : '' ;
 });
 
 contactBtn.addEventListener("click", () => {
   content.removeChild(content.lastElementChild);
   addContact();
+  !contactBtn.classList.contains("active") ? contactBtn.classList.toggle("active") : '' ;
+  menuBtn.classList.contains("active") ? menuBtn.classList.toggle("active") : '' ;
+  homeBtn.classList.contains("active") ? homeBtn.classList.toggle("active") : '' ;
 });
